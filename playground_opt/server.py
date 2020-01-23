26ec11d80edbd343cfb73f022d9c58226f3df9ca
Tanner Smith <57360812+bedbathburritos@users.noreply.github.com>
2020-01-23 15:03:57 -0600

    Delete server.py

--- playground_opt/server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/play/<times>/<color>')
-def index(times, color):
-    return render_template('index.html', box_number=int(times), box_color=color)
-
-if __name__ == "__main__":
-    app.run(debug=True)
